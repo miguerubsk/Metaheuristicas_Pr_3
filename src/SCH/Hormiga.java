0f275947c9f0ce27a0b9b959a1874aacca748923
Miguerubsk <miguerubsk@gmail.com>
2020-12-14 16:25:30 +0100

    Update Hormiga.java

--- src/SCH/Hormiga.java
+++ src/SCH/Hormiga.java
@@ -5,10 +5,46 @@
  */
 package SCH;
 
+import java.util.Vector;
+import tools.CargaDatos;
+import tools.Random;
+
 /**
  *
  * @author Miguerubsk
  */
 public class Hormiga {
+    
+    private Vector<Integer> sol;
+    private int tamSol;
+    private double coste;
+    private Random aleatorio;
+    private CargaDatos datos;
+    
+    public Hormiga(long semilla, CargaDatos datos){
+        this.datos = datos;
+        this.aleatorio = new Random(semilla);
+        this.tamSol = this.datos.getTamSolucion();
+        this.sol = new Vector<Integer>();
+        this.coste = coste(this.datos);
+    }
+    
+    /**
+     * @brief Función que calcula el coste de la solucion
+     * @param matriz matriz de distancias
+     * @param tamañoSolucion tamaño de la solucion
+     * @return Coste de la solucion
+     */
+    private double coste(CargaDatos datos) {
+        double coste = 0.0;
+
+        for (int i = 0; i < sol.size(); i++) {
+            for (int j = i + 1; j < sol.size(); j++) {
+                coste += datos.getMatriz()[sol.get(j)][sol.get(i)];
+            }
+        }
+
+        return coste;
+    }
     
 }
