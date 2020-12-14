e26dc41a0e14d41cfb59fe8a796366fb9faa4dd7
Miguerubsk <miguerubsk@gmail.com>
2020-12-14 16:14:57 +0100

    Update Poblacion.java

--- src/SCH/Poblacion.java
+++ src/SCH/Poblacion.java
@@ -5,10 +5,34 @@
  */
 package SCH;
 
+import java.util.Vector;
+import tools.CargaDatos;
+import tools.Configurador;
+import tools.Random;
+
 /**
  *
  * @author Miguerubsk
  */
 public class Poblacion {
+    
+    private Vector<Hormiga> poblacion;
+    private int tamPoblacion;
+    private long semilla;
+    private CargaDatos datos;
+    private final Configurador config;
+    private Random aleatorio;
+    
+    public Poblacion(long semilla, CargaDatos datos, boolean generar, Configurador config) {
+        this.poblacion = new Vector<Hormiga>();
+        this.tamPoblacion = 0;
+        this.semilla = semilla;
+        this.datos = datos;
+        this.config = config;
+        this.aleatorio = new Random(semilla);
+        if (generar) {
+//            generarPoblacion();
+        }
+    }
     
 }
