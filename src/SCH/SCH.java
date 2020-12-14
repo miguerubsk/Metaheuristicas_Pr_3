ed152ff348afd8a07073eb02adfd6659e27d0c7f
Miguerubsk <miguerubsk@gmail.com>
2020-12-14 16:15:01 +0100

    Update SCH.java

--- src/SCH/SCH.java
+++ src/SCH/SCH.java
@@ -5,10 +5,28 @@
  */
 package SCH;
 
+import tools.CargaDatos;
+import tools.Configurador;
+
 /**
  *
  * @author Miguerubsk
  */
 public class SCH {
     
+    private final CargaDatos datos; //Datos para realizar la ejecucion
+    private final Configurador config; //Archivo de configuracion
+    private final Long semilla; //Semilla para inicializacion del aleatorio
+    private Poblacion poblacion;
+    
+    public SCH(CargaDatos data, Configurador conf, Long seed){
+         this.datos = data;
+         this.config = conf;
+         this.semilla = seed;
+         this.poblacion = new Poblacion(semilla, datos, true, config);
+    }
+    
+    public void ejecutar() throws Exception{
+        //TODO
+    }
 }
